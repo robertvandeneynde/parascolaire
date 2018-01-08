e6163b2c2357ef296974906084206c2f089217e2
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2018-01-08 10:25:16 +0100

    auto

--- vec3_utils.py
+++ vec3_utils.py
@@ -1,8 +1,6 @@
-from OpenGL.GL import *
-from OpenGL.GL import shaders
-import ctypes
-import pygame
-from math import sin,cos,degrees,radians,tan
+from __future__ import division
+
+from math import sin, cos, degrees, radians, tan
 
 import numpy
 from numpy import array, matrix, linalg
@@ -10,17 +8,22 @@ from numpy import array, matrix, linalg
 def farray(*args):
     return numpy.array(*args, dtype=numpy.float32)
 
-class Axis:
-    pass
+def sind(x):
+    return sin(radians(x))
 
-Axis.X = 0
-Axis.Y = 1
-Axis.Z = 2
+def cosd(x):
+    return cos(radians(x))
+
+def tand(x):
+    return tan(radians(x))
 
 def polar(*args):
-    if len(args) == 2: r,t = args
-    elif len(args) == 1: r,t = 1,args[0]
-    else: raise TypeError('Accept 1 or 2 arguments')
+    if len(args) == 2:
+        r,t = args
+    elif len(args) == 1:
+        r,t = 1, args[0]
+    else:
+        raise TypeError('Accept 1 or 2 arguments')
     
     return r * vec2(cos(t), sin(t))
 
@@ -38,7 +41,24 @@ NAME_TO_INT = {
     'x': 0,
     'y': 1,
     'z': 2,
+    'w': 3,
+    'r': 0,
+    'g': 1,
+    'b': 2,
+    'a': 3,
 }
+
+def readvec(V, names):
+    """
+    Returns numpy.array from values of vector V depending on arguments:
+    readvec((1,2,3), 'xy') -> farray((1,2))
+    readvec((1,2,3), 'rg') -> farray((1,2))
+    readvec((1,2,3), 'zx') -> farray((3,1))
+    readvec((1,2,3), 'zz') -> farray((3,3))
+    """
+    return farray([
+        V[NAME_TO_INT[x]]
+        for i,x in enumerate(names)])
 
 def vec3(*args, **kwargs):
     '''
@@ -82,7 +102,7 @@ def vec3(*args, **kwargs):
         elif len(args) == 0:
             x,y,z = 0,0,0
         else:
-            raise TypeError('Accept 1 2 or 3 arguments')
+            raise TypeError('Accept 0, 1, 2 or 3 arguments')
     
         return array((x,y,z), dtype=numpy.float32)
 
@@ -90,26 +110,30 @@ def normalized(v):
     return v / linalg.norm(v)
 
 def PerpectiveMatrix(fovy, aspect, zNear, zFar):
-    f = 1.0/tan(radians(fovy)/2.0)
-    return matrix([
+    f = 1.0 / tan(radians(fovy) / 2.0)
+    return farray([
             [f/aspect, 0, 0, 0],
             [0, f, 0, 0],
-            [0, 0, 1. * (zFar + zNear) / (zNear - zFar), 2.0 * zFar * zNear / (zNear - zFar)],
+            [0, 0, 1.0 * (zFar + zNear) / (zNear - zFar), 2.0 * zFar * zNear / (zNear - zFar)],
             [0,0,-1,0]
-        ], dtype=numpy.float32)
+        ])
 
 def TranslationMatrix(*args):
-    if len(args) == 3: tx,ty,tz = args
-    elif len(args) == 2: (tx,ty),tz = args, 0
-    elif len(args) == 1: tx,ty,tz = args[0]
-    else: raise TypeError("Accept 1, 2 or 3 arguments")
+    if len(args) == 3:
+        tx,ty,tz = args
+    elif len(args) == 2:
+        (tx,ty),tz = args, 0
+    elif len(args) == 1:
+        tx,ty,tz = args[0]
+    else:
+        raise TypeError("Accept 1, 2 or 3 arguments")
         
-    return matrix([
+    return farray([
             [1, 0, 0, tx],
             [0, 1, 0, ty],
             [0, 0, 1, tz],
             [0, 0, 0, 1]
-        ], dtype=numpy.float32)
+        ])
 
 def LookAtMatrix(*args):
     if len(args) == 3:
@@ -125,14 +149,19 @@ def LookAtMatrix(*args):
     s = numpy.cross(f, U)
     u = numpy.cross(normalized(s), f)
     
-    return matrix([
+    return farray([
         [ s[0],  s[1],  s[2], 0],
         [ u[0],  u[1],  u[2], 0],
         [-f[0], -f[1], -f[2], 0],
         [    0,     0,     0, 1],
-    ], dtype=numpy.float32).dot(
+    ]).dot(
         TranslationMatrix(-e)
     )
+
+class Axis:
+    X = 0
+    Y = 1
+    Z = 2
 
 def AxeRotationMatrix(angle, axe=Axis.Z):
     if angle % 90 == 0:
@@ -144,7 +173,7 @@ def AxeRotationMatrix(angle, axe=Axis.Z):
         c = cos(t)
         s = sin(t)
     
-    return matrix([
+    return farray([
             [c, s, 0, 0],
             [-s, c, 0, 0],
             [0, 0, 1, 0],
@@ -159,88 +188,4 @@ def AxeRotationMatrix(angle, axe=Axis.Z):
             [0, c, -s, 0],
             [0, s, c, 0],
             [0, 0, 0, 1]
-        ], dtype=numpy.float32)
-
-def create_object(shader):
-    vertex_array_object = glGenVertexArrays(1)
-    glBindVertexArray( vertex_array_object )
-    
-    vertex_buffer = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)
-    
-    position = glGetAttribLocation(shader, 'position')
-    glEnableVertexAttribArray(position)
-    
-    glVertexAttribPointer(position, 4, GL_FLOAT, False, 0, ctypes.c_void_p(0))
-    
-    glBufferData(GL_ARRAY_BUFFER, ArrayDatatype.arrayByteCount(vertices), vertices, GL_STATIC_DRAW)
-    
-    glBindVertexArray(0)
-    
-    glDisableVertexAttribArray(position)
-    glBindBuffer(GL_ARRAY_BUFFER, 0)
-    
-    return vertex_array_object
-    
-def display(shader, vertex_array_object, t):
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glUseProgram(shader)
-    
-    # build projection matrix
-    p = PerpectiveMatrix(45, 1.0 * 512/512, 0.1, 100)
-    
-    # build view matrix (lookAt)
-    v = LookAtMatrix(vec3(xy = 7 * polar(0.5 * t), z = 5), (0, 0, 0), (0, 0, 1))
-    pv = p.dot(v)
-    
-    # build model matrix of triangles (boucy animation)
-    m = TranslationMatrix(0, 0, abs(sin(3*t)))
-    pvm = pv.dot(m)
-    
-    # use our only vao
-    glBindVertexArray(vertex_array_object)
-    
-    # set matrix
-    loc_matrix = glGetUniformLocation(shader, 'pvmMatrix')
-    glUniformMatrix4fv(loc_matrix, 1, False, pvm.T)
-    
-    # draw
-    glPointSize(5)
-    glDrawArrays(GL_TRIANGLES, 0, 18)
-    glBindVertexArray(0)
-    
-    glUseProgram(0)
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((512, 512), pygame.OPENGL|pygame.DOUBLEBUF)
-    glClearColor(0.5, 0.5, 0.5, 1.0)
-    glViewport(0, 0, 512, 512)
-    glEnable(GL_DEPTH_TEST)
-
-    shader = shaders.compileProgram(
-        shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
-        shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER)
-    )
-    
-    vertex_array_object = create_object(shader)
-    
-    clock = pygame.time.Clock()
-    
-    t = 0
-    done = False
-    while not done:     
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        
-        t += 1
-        display(shader, vertex_array_object, t/60.)
-        pygame.display.flip()
-        clock.tick(60)
-
-if __name__ == '__main__':
-    try:
-        main()
-    finally:
-        pygame.quit()
+        ])
